04200c12cfec2dc0cdcab5d104d5d507a9bbb2f4
thomascsd <thomascsd808@gmail.com>
2020-08-27 23:15:26 +0800

    02guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,26 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Please input your number !");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Input number");
+    let guess: u32 = guess.trim().parse().expect("It's not a number!!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+}
